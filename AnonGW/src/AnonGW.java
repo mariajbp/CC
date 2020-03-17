2eeee637f3018b469c05ce1e2c39d9b9ad9700f6
Susana <susymarques99@gmail.com>
2020-03-17 12:20:17 +0000

    up

--- AnonGW/src/AnonGW.java
+++ AnonGW/src/AnonGW.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+
 public class AnonGW
 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("main bonita só para ti Hugo");
+    }
 }
